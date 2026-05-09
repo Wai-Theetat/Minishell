7c36ebc49d612d1f42a6b5a430bd662e9020288f
Waiton <theetat.ae@gmail.com>
2026-05-09 13:55:51 +0700

    fix norminette

--- srcs/lexer/lexer_word.c
+++ srcs/lexer/lexer_word.c
@@ -6,7 +6,7 @@
 /*   By: tdharmar <tdharmar@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 13:28:29 by tdharmar          #+#    #+#             */
-/*   Updated: 2026/05/09 13:29:11 by tdharmar         ###   ########.fr       */
+/*   Updated: 2026/05/09 13:55:40 by tdharmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,50 +41,52 @@ static int	word_len(const char *input, int i)
 	return (len);
 }
 
-static void	fill_buf(const char *input, int *i, char *buf, int *j, char *q)
+static void	handle_quote(const char *input, int *pos, char *quote, char *q)
+{
+	if (*q == 0)
+		*q = input[*pos];
+	else if (*q != input[*pos])
+		*q = 0;
+	*quote = input[(*pos)++];
+}
+
+static void	fill_buf(const char *input, int *pos, t_buf *buf, char *q)
 {
 	char	quote;
 
 	quote = 0;
-	while (input[*i])
+	while (input[*pos])
 	{
-		if (!quote && (input[*i] == '\'' || input[*i] == '"'))
-		{
-			if (*q == 0)
-				*q = input[*i];
-			else if (*q != input[*i])
-				*q = 0;
-			quote = input[(*i)++];
-		}
-		else if (quote && input[*i] == quote)
+		if (!quote && (input[*pos] == '\'' || input[*pos] == '"'))
+			handle_quote(input, pos, &quote, q);
+		else if (quote && input[*pos] == quote)
 		{
 			quote = 0;
-			(*i)++;
+			(*pos)++;
 		}
-		else if (ft_iswordend(input[*i], quote))
+		else if (ft_iswordend(input[*pos], quote))
 			break ;
 		else
-			buf[(*j)++] = input[(*i)++];
+			buf->data[(buf->pos)++] = input[(*pos)++];
 	}
 }
 
-static char	*collect_word(const char *input, int *i, char *quote_out)
+static char	*collect_word(const char *input, int *pos, char *quote_out)
 {
-	char	*buf;
+	t_buf	buf;
 	int		len;
-	int		j;
 
-	len = word_len(input, *i);
+	len = word_len(input, *pos);
 	if (len == -1)
 		return (NULL);
-	buf = ft_gc_malloc(len + 1);
-	if (!buf)
+	buf.data = ft_gc_malloc(len + 1);
+	if (!buf.data)
 		return (NULL);
-	j = 0;
+	buf.pos = 0;
 	*quote_out = 0;
-	fill_buf(input, i, buf, &j, quote_out);
-	buf[j] = '\0';
-	return (buf);
+	fill_buf(input, pos, &buf, quote_out);
+	buf.data[buf.pos] = '\0';
+	return (buf.data);
 }
 
 t_token	*ft_lex_word(const char *input, int *i)
