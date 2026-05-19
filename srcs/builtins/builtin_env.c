f16b4bbf7789c49d8a418bdf4e28187a3f67fbd0
ChevFy <koonchevychpai123@gmail.com>
2026-05-19 13:23:02 +0700

    buitin-misc -> env,pwd

--- srcs/builtins/builtin_env.c
+++ srcs/builtins/builtin_env.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtin_main.c                                     :+:      :+:    :+:   */
+/*   builtin_misc.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: koonchevychpai123 <koonchevychpai123@st    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/09 16:23:14 by koonchevych       #+#    #+#             */
-/*   Updated: 2026/05/09 16:37:26 by koonchevych      ###   ########.fr       */
+/*   Updated: 2026/05/19 13:21:56 by koonchevych      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,6 @@ int	builtin_env(t_env *env)
 		ft_printf("%s=%s\n", env->key, env->value);
 		env = env->next;
 	}
-	return (0);
-}
-
-int	buitin_pwd(void)
-{
-	char	buffer_path[1024];
-
-	if (!getcwd(buffer_path, 1024))
-	{
-		perror("pwd");
-		return (1);
-	}
-	ft_printf("%s\n", buffer_path);
 	return (0);
 }
 
