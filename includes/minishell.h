1a835b515a6aae9878c929ad0a395209c362c3ad
ChevFy <koonchevychpai123@gmail.com>
2026-06-22 09:51:51 +0700

    Fix include order in minishell.h and update buitlin_cd.c header comment

--- includes/minishell.h
+++ includes/minishell.h
@@ -17,11 +17,11 @@
 # include "structs.h"
 # include <errno.h>
 # include <fcntl.h>
-# include <readline/history.h>
-# include <readline/readline.h>
 # include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <readline/history.h>
+# include <readline/readline.h>
 # include <sys/wait.h>
 # include <unistd.h>
 
